912d7bc0a5b8d4d845b82d62ef1ca758922e17d9
Omer Klein <51273463+omerklein1@users.noreply.github.com>
2022-01-17 18:45:07 +0200

    1.0

--- server/BL/index.js
+++
@@ -1,5 +0,0 @@
-const DL_Mongo = require("../DL_Mongo"),
-  users = require("./users")(DL_Mongo),
-  heroes = require("./heroes")(DL_Mongo);
-
-module.exports = { users, heroes };
--- server/DL_Mongo/models/UsersModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true, index: { unique: true } },
-  password: { type: String, required: true },
-});
-
-module.exports = mongoose.model("users", userSchema);
